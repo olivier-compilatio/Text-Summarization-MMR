14146f4097ac69c6cc5a70cd80705b24553b084f
olivier-compilatio <olivier@compilatio.net>
2019-03-29 11:28:04 +0100

    refactoring of the code working with indian

--- mmr.py
+++ mmr.py
@@ -1,118 +1,140 @@
 import os
 import re
 import sys
+
+from termcolor import colored
+
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 import operator
 
-# create stemmer
-factory = StemmerFactory()
-stemmer = factory.create_stemmer()
-
-def load_stopWords():
-	f = file('stopword.txt', 'r');
-	return f.readlines()
-
-stopwords = load_stopWords()	
 
 def cleanData(sentence):
-	#sentence = re.sub('[^A-Za-z0-9 ]+', '', sentence)
-	#sentence filter(None, re.split("[.!?", setence))
-	ret = []
-	sentence = stemmer.stem(sentence)	
-	for word in sentence.split():
-		if not word in stopwords:
-			ret.append(word)
-	return " ".join(ret)
+    # sentence = re.sub('[^A-Za-z0-9 ]+', '', sentence)
+    # sentence filter(None, re.split("[.!?", setence))
+    ret = []
+    sentence = stemmer.stem(sentence)
+    for word in sentence.split():
+        if not word in stopwords:
+            ret.append(word)
+    return " ".join(ret)
 
 
 def getVectorSpace(cleanSet):
-	vocab = {}
-	for data in cleanSet:
-		for word in data.split():
-			vocab[data] = 0
-	return vocab.keys()
-	
+    vocab = {}
+    for data in cleanSet:
+        for word in data.split():
+            vocab[data] = 0
+    return vocab.keys()
+
+
 def calculateSimilarity(sentence, doc):
-	if doc == []:
-		return 0
-	vocab = {}
-	for word in sentence:
-		vocab[word] = 0
-	
-	docInOneSentence = '';
-	for t in doc:
-		docInOneSentence += (t + ' ')
-		for word in t.split():
-			vocab[word]=0	
-	
-	cv = CountVectorizer(vocabulary=vocab.keys())
+    if doc == []:
+        return 0
+    vocab = {}
+    for word in sentence:
+        vocab[word] = 0
 
-	docVector = cv.fit_transform([docInOneSentence])
-	sentenceVector = cv.fit_transform([sentence])
-	return cosine_similarity(docVector, sentenceVector)[0][0]
-	
+    docInOneSentence = ""
+    for t in doc:
+        docInOneSentence += t + " "
+        for word in t.split():
+            vocab[word] = 0
 
-data = file(sys.argv[1], 'r')
-texts = data.readlines()
+    cv = CountVectorizer(vocabulary=vocab.keys())
 
-sentences = []
-clean = []
-originalSentenceOf = {}
-	
-import time
-start = time.time()
-
-#Data cleansing
-for line in texts:
-	parts = line.split('.')
-	for part in parts:
-		cl = cleanData(part)
-		#print cl
-		sentences.append(part)
-		clean.append(cl)
-		originalSentenceOf[cl] = part		
-setClean = set(clean)
-		
-#calculate Similarity score each sentence with whole documents		
-scores = {}
-for data in clean:
-	temp_doc = setClean - set([data])
-	score = calculateSimilarity(data, list(temp_doc))
-	scores[data] = score
-	#print score
+    docVector = cv.fit_transform([docInOneSentence])
+    sentenceVector = cv.fit_transform([sentence])
+    return cosine_similarity(docVector, sentenceVector)[0][0]
 
 
-#calculate MMR
-n = 20 * len(sentences) / 100
-alpha = 0.5
-summarySet = []
-while n > 0:
-	mmr = {}
-	#kurangkan dengan set summary
-	for sentence in scores.keys():
-		if not sentence in summarySet:
-			mmr[sentence] = alpha * scores[sentence] - (1-alpha) * calculateSimilarity(sentence, summarySet)	
-	selected = max(mmr.iteritems(), key=operator.itemgetter(1))[0]	
-	summarySet.append(selected)
-	n -= 1
+def load_stopwords(stopwords_file):
+    f = file("stopword.txt", "r")
+    return f.readlines()
 
-#rint str(time.time() - start)
-	
-print '\nSummary:\n'
-for sentence in summarySet:
-	print originalSentenceOf [sentence].lstrip(' ')
-print
 
-print '============================================================='
-print '\nOriginal Passages:\n'
-from termcolor import colored
+def load_sentences(text_file):
+    data = file(sys.argv[1], "r")
+    texts = data.readlines()
 
-for sentence in clean:
-	if sentence in summarySet:
-		print colored(originalSentenceOf[sentence].lstrip(' '), 'red')
-	else:
-		print originalSentenceOf[sentence].lstrip(' ')
-	
+    sentences = []
+    clean = []
+    originalSentenceOf = {}
+    # Data cleansing
+    for line in texts:
+        parts = line.split(".")
+        for part in parts:
+            cl = cleanData(part)
+            # print cl
+            sentences.append(part)
+            clean.append(cl)
+            originalSentenceOf[cl] = part
+    setClean = set(clean)
+
+    return setClean, originalSentenceOf, sentences, clean
+
+
+def load_data(stopwords_file, text_file):
+    stopwords = load_stopwords(stopwords_file)
+    setClean, originalSentenceOf, sentences, clean = load_sentences(text_file)
+
+    return stopwords, setClean, originalSentenceOf, sentences, clean
+
+
+def compute_similarity_scores(clean, setClean):
+    # calculate Similarity score each sentence with whole documents
+    scores = {}
+    for data in clean:
+        temp_doc = setClean - set([data])
+        score = calculateSimilarity(data, list(temp_doc))
+        scores[data] = score
+    return scores
+
+
+def summarize_mmr(scores):
+    # calculate MMR
+    n = 20 * len(sentences) / 100
+    alpha = 0.5
+    summarySet = []
+    while n > 0:
+        mmr = {}
+        for sentence in scores.keys():
+            if not sentence in summarySet:
+                mmr[sentence] = alpha * scores[sentence] - (
+                    1 - alpha
+                ) * calculateSimilarity(sentence, summarySet)
+        selected = max(mmr.iteritems(), key=operator.itemgetter(1))[0]
+        summarySet.append(selected)
+        n -= 1
+    return summarySet
+
+
+def show_results(summarySet):
+
+    print("\nSummary:\n")
+    for sentence in summarySet:
+        print(originalSentenceOf[sentence].lstrip(" "))
+    print()
+
+    print("=============================================================")
+    print("\nOriginal Passages:\n")
+    for sentence in clean:
+        if sentence in summarySet:
+            print(colored(originalSentenceOf[sentence].lstrip(" "), "red"))
+        else:
+            print(originalSentenceOf[sentence].lstrip(" "))
+
+
+def main():
+    stopwords_file = "stopwords.json"
+    text_file = "tosummarize.txt"
+    stopwords, setClean, originalSentenceOf, sentences, clean = load_data(
+        stopwords_file, text_file
+    )
+    factory = StemmerFactory()
+    stemmer = factory.create_stemmer()
+    scores = compute_similarity_scores(clean, setClean)
+    summary = summarize_mmr(scores)
+    show_results(summary)
